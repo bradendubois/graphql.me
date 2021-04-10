ec6288bcac7654a8aa968e13483302813b7bbac4
Braden Dubois <braden.dubois@usask.ca>
2021-04-10 13:46:43 -0600

    cleaned index.js, added open graph meta properties

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,26 @@
 import Head from 'next/head'
-import useSWR from "swr"
-import styles from '../styles/Home.module.css'
-
-import React from 'react';
 import GraphiQL from 'graphiql';
 import "graphiql/graphiql.min.css"
 
 import "../styles/Home.module.css"
 
-/*
-export default function Home() {
-
-  useSWR("/api/hello")
-
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
-*/
-
 const Home = () => (
 
     <div>
-        <Head></Head>
+        <Head>
+            <title>GraphQL | api.bradendubois.dev</title>
+            <meta charSet="UTF-8" />
+            <meta name="description" content="Free Web tutorials" />
+            <meta property="og:title" content="GraphQL | api.bradendubois.dev" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://www.api.bradendubois.dev" />
+        </Head>
 
         <main>
             <GraphiQL
                 fetcher={async graphQLParams => {
                     const data = await fetch(
-                        '/api/hello',
+                        '/api/graphql',
                         {
                             method: 'POST',
                             headers: {
