a438252c612760ee2429916a7b45b744c546eb16
Braden Dubois <braden.dubois@usask.ca>
2021-04-11 21:46:58 -0600

    add error handling

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,7 +3,10 @@ import { graphql } from "graphql"
 
 
 export default (req, res) => {
-  graphql(Schema, req.body.query).then(result => {
-    res.status(200).json(result)
-  })
+
+    console.log(req.body)
+
+    graphql(Schema, req.body.query)
+        .then(result => { res.status(200).json(result) })
+        .catch(err => { res.status(400).json(err) })
 }
