61832e1cf796ec0d0d85bc4468721828efdbeaed
Braden Dubois <braden.dubois@usask.ca>
2021-04-11 21:48:00 -0600

    removed unnecessary console log

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,9 +3,6 @@ import { graphql } from "graphql"
 
 
 export default (req, res) => {
-
-    console.log(req.body)
-
     graphql(Schema, req.body.query)
         .then(result => { res.status(200).json(result) })
         .catch(err => { res.status(400).json(err) })
